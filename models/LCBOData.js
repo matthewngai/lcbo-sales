9c2427fa52236c83b1673a314274a87ab2e27af0
eho <ernestho.ho@gmail.com>
2016-01-17 21:47:50 -0500

    Get product data with cUrl

--- models/LCBOData.js
+++ models/LCBOData.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var execFile = require('child_process').execFile;
 var mongoose = require('mongoose');
 var async = require('async');
 var _ = require('lodash');
@@ -18,52 +18,20 @@ var form = {
 
 var LCBOData = {};
 
-LCBOData.test2 = function() {
-  request.post({
-   url: url,
-   qs: form,
-   headers: {
-     'cache-control': 'no-cache'
-   }
-  }, function(err, response, body) {
-   if (err) {
-     console.error(err);
-     return;
-   }
-   console.log(body);
-  });
-}
-
-// var request = require("request");
-
 LCBOData.test = function() {
-  var http = require("http");
+  var method = '-X POST';
+  var headers = '-H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded"';
+  var data = '-d "contentBeginIndex=0&productBeginIndex=50&beginIndex=50&orderBy=&categoryPath=%2F%2F&pageView=&resultType=&orderByContent=&searchTerm=&facet=&storeId=10151&catalogId=10001&langId=-1&fromPage=&objectId=&requesttype=ajax"';
+  var url = 'http://www.lcbo.com/webapp/wcs/stores/servlet/CategoryNavigationResultsView?pageSize=50&manufacturer=&searchType=&resultCatEntryType=&catalogId=10001&categoryId=&langId=-1&storeId=10151&sType=SimpleSearch&filterFacet=&metaData=';
 
-  var options = {
-    "method": "POST",
-    "hostname": "www.lcbo.com",
-    "port": null,
-    "path": "/webapp/wcs/stores/servlet/CategoryNavigationResultsView?pageSize=50&manufacturer=&searchType=&resultCatEntryType=&catalogId=10001&categoryId=&langId=-1&storeId=10151&sType=SimpleSearch&filterFacet=&metaData=",
-    "headers": {
-      "cache-control": "no-cache",
-      "postman-token": "5f029b66-c3f7-e8e2-1305-ca38394b57dc"
+  var args = [method, data, headers, url];
+
+  execFile('curl.exe', args, {}, function (error, stdout, stderr) {
+    if (error) {
+      console.log('exec error: ' + error);
     }
-  };
-
-  var req = http.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-
-    res.on("end", function () {
-      var body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
+    console.log(stdout);
   });
-
-  req.end();
 }
 
 module.exports = LCBOData;
