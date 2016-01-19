a3a1cc7f9897e9bf7e56bbf6bb324403cba99ee1
eho <ernestho.ho@gmail.com>
2016-01-18 20:52:11 -0500

    Update readme, add angular material css

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 `npm install`  
 `set DEBUG=myapp:* && npm start`
 
-## Api Calls
-`/api/products?pageNum=0&pageSize=10`  
-`/api/sales`
+## Api Call
+`/api/sales?pageNum=0&pageSize=10`
--- routes/api.js
+++ routes/api.js
@@ -3,42 +3,46 @@ var router = express.Router();
 
 var LCBOData = require('../modules/LCBOData');
 
-var getPageSize(req) {
-	return req.query.pageNum || 0;
+var getPageSize = function(req) {
+  return req.query.pageNum || 0;
 }
 
-var getPageNum(req) {
-	return req.query.pageSize || 10;
+var getPageNum = function(req) {
+  return req.query.pageSize || 10;
 }
 
 // /api/products?pageNum=Number&pageSize=Number&orderBy=(todo)&filter=(todo)
-router.get('/products', function(req, res, next) {
-  var pageNum = getPageNum(req);
-  var pageSize = getPageSize(req);
+// router.get('/products', function(req, res, next) {
+//   var pageNum = getPageNum(req);
+//   var pageSize = getPageSize(req);
 
-  var results = LCBOData.get(pageNum, pageSize)
-	  .then(products => {
-	  	LCBOData.getCount()
-	  		.then(count => {
-		  		res.set('totalCount', count);
-			  	res.send(products);
-		  	});
-	  }, err => {
-	  	res.sendStatus(500);
-	  });
-});
+//   var results = LCBOData.get(pageNum, pageSize)
+//     .then(products => {
+//       LCBOData.getCount()
+//         .then(count => {
+//           res.set('totalCount', count);
+//           res.send(products);
+//         });
+//     }, err => {
+//       res.sendStatus(500);
+//     });
+// });
 
 // /api/sales?pageNum=Number&pageSize=Number
 router.get('/sales', function(req, res, next) {
-	var pageNum = getPageNum(req);
+  var pageNum = getPageNum(req);
   var pageSize = getPageSize(req);
 
-	LCBOData.getOnSale(pageNum, pageSize)
-		.then(products => {
-			res.send(products);
-		}, err => {
-			res.sendStatus(500);
-		});
+  LCBOData.getOnSale(pageNum, pageSize)
+    .then(products => {
+      LCBOData.getCount()
+        .then(count => {
+          res.set('totalCount', count);
+          res.send(products);
+        });
+    }, err => {
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
