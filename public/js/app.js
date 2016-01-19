1744a6ad991676bcf366c3bc021b4bcdd5d486e3
eho <ernestho.ho@gmail.com>
2016-01-19 05:19:31 -0500

    Add paging buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,59 +1,60 @@
 define(['material', 'dataService'], function() {
   angular.module('app', ['ngMaterial', 'lsService'])
-    .controller('AppController', function(lsDataService) {
-      var vm = this;
+  .controller('AppController', function(lsDataService) {
+    var vm = this;
 
-      vm.products = [];
-      vm.count = 0;
-      vm.pageSize = 10;
-      vm.pageNumber = 0;
-      vm.productView = [];
-      
-      vm.onPageResize = function(){
-        var productLength = vm.products.length;
-      	var limit = vm.pageSize * (vm.pageNumber + 1) - vm.products.length;
-        if (limit > 0) {
-          var offset = productLength;
-          vm.updateProducts(offset, limit);
-        } 
-        else {
-          vm.updateProductView();
+    vm.products = [];
+    vm.count = 0;
+    vm.pageSize = 10;
+    vm.pageNumber = 0;
+    
+    vm.previousPage = function() {
+      vm.pageNumber--;
+      vm.onPageResize();
+    }
+
+    vm.nextPage = function() {
+      vm.pageNumber++;
+      vm.onPageResize();
+    }
+
+    vm.onPageResize = function() {
+      var productLength = vm.products.length;
+      var limit = vm.pageSize * (vm.pageNumber + 1) - vm.products.length;
+      if (limit > 0) {
+        var offset = productLength;
+        vm.updateProducts(offset, limit);
+      }
+    }
+
+    vm.updateProducts = function(offset, limit) {
+      lsDataService.getSales(offset, limit).then(function(data) {
+        vm.count = data.count;
+        vm.products = vm.products.concat(data.products);
+      });
+    }
+    vm.updateProducts(vm.pageNumber * vm.pageSize, vm.pageSize);
+  })
+  .directive('lsTable',function() {
+    return {
+      restrict: 'E',
+      scope: {
+        products: '=lsProducts'
+      },
+      controller: function(lsConstants) {
+        var vm = this;
+        vm.getLink = function(link) {
+          return lsConstants.LCBO_URL + link;
         }
-      }
-
-      vm.updateProductView = function() {
-        vm.productView = vm.products.slice(vm.pageSize*vm.pageNumber, vm.pageSize*(vm.pageNumber+1));
-      }
-
-      vm.updateProducts = function(offset, limit) {
-        lsDataService.getSales(offset, limit).then(function(data) {
-          vm.count = data.count;
-          vm.products = vm.products.concat(data.products);
-          vm.updateProductView();
-        });
-      }
-      vm.updateProducts(vm.pageNumber * vm.pageSize, vm.pageSize);
-    })
-    .directive('lsTable',function(){
-    	return {
-    		restrict: 'E',
-    		scope: {
-    			products: '=lsProducts'
-    		},
-    		controller: function(lsConstants) {
-    			var vm = this;
-    			vm.getLink = function(link) {
-    				return lsConstants.LCBO_URL + link;
-    			}
-    		},
-    		bindToController: true,
-    		controllerAs: 'vm',
-    		templateUrl: 'templates/table.html'
-    	};
-    })
-    .config(function($mdThemingProvider) {
-      $mdThemingProvider.theme('default')
-        .primaryPalette('green')
-        .accentPalette('blue');
-    });
+      },
+      bindToController: true,
+      controllerAs: 'vm',
+      templateUrl: 'templates/table.html'
+    };
+  })
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('green')
+      .accentPalette('blue');
+  });
 });
