123d17818523d32d001d54ae8efd9dfc02385429
Chaitanya Khanna <c2khanna@uwaterloo.ca>
2016-01-19 02:26:55 -0500

    added table.html

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,31 @@ define(['material', 'dataService'], function() {
 
       vm.products = [];
       vm.count = 0;
-
       vm.getProducts = function() {
         // Page Number, Page Size
-        lsDataService.getSales(0, 4).then(function(data) {
+        lsDataService.getSales(0, 165).then(function(data) {
           vm.count = data.count;
           vm.products = data.products;
         });
       }
       vm.getProducts();
+    })
+    .directive('lsTable',function(){
+    	return {
+    		restrict: 'E',
+    		scope: {
+    			products: '=lsProducts'
+    		},
+    		controller: function(lsConstants) {
+    			var vm = this;
+    			vm.getLink = function(link) {
+    				return lsConstants.LCBO_URL + link;
+    			}
+    		},
+    		bindToController: true,
+    		controllerAs: 'vm',
+    		templateUrl: 'templates/table.html'
+    	};
     })
     .config(function($mdThemingProvider) {
       $mdThemingProvider.theme('default')
